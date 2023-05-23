6109a4d38f6a9c08c539915dd8f4aa18c88085bf
Daniel Irene <pdirene@utpl.edu.ec>
2023-05-23 12:51:29 -0500

    Eliminando espacio y comentarios de las clases para mejorar visualización

--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete1/LibretaCalificacion.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete1/LibretaCalificacion.java
@@ -1,39 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete1;
-
-/**
- *
- * @author reroes
- */
 public class LibretaCalificacion {
     private String estudiante;
     private double promedio;
     private String promedioCualitativo;
     private Calificacion [] calificaciones;
-    
     public LibretaCalificacion(String n, Calificacion [] c){
         estudiante = n;
         calificaciones = c;
     }
-    
     public void establecerEstudiante(String n){
         estudiante = n;
     }
-    
     public void establecerPromedio(){
         double suma = 0;
-        
         for (int i = 0; i < obtenerCalificaciones().length; i++) {
             suma = suma + obtenerCalificaciones()[i].obtenerNota();
         }
-        
         promedio = suma/obtenerCalificaciones().length;
     }
-    
     public void establecerPromedioCualitativo(){
         if((obtenerPromedio()>=0)&&(obtenerPromedio()<=3.09)){
             promedioCualitativo = "Regular";
@@ -57,27 +41,21 @@ public class LibretaCalificacion {
             }
         }
     }
-    
     public void establecerCalificaciones(Calificacion [] n){
         calificaciones = n;
     }
-    
     public String obtenerEstudiante(){
         return estudiante;
     }
-    
     public double obtenerPromedio(){
         return promedio;
     }
-    
     public String obtenerPromedioCualitativo(){
         return promedioCualitativo;
     }
-    
     public Calificacion [] obtenerCalificaciones(){
         return calificaciones;
     }
-    
     @Override
     public String toString(){
         String cadena = "Libreta de Calificaciones\n";
--- EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete2/ArchivoLectura.java
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete2/ArchivoLectura.java
@@ -1,25 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import paquete1.Profesor;
-
 public class ArchivoLectura {
-
     private Scanner entrada;
     private String nombreArchivo;
     private String rutaArchivo;
     private ArrayList<Profesor> lista;
-
     public ArchivoLectura(String n) {
         nombreArchivo = n;
         rutaArchivo = String.format("data/%s", nombreArchivo);
@@ -33,39 +23,30 @@ public class ArchivoLectura {
             } // fin de try
             catch (FileNotFoundException e) {
                 System.err.println("Error al leer del archivo: " + e);
-
             } // fin de catch
         }
     }
-
     public void establecerNombreArchivo(String n) {
         nombreArchivo = n;
     }
-
     public void establecerRutaArchivo() {
         rutaArchivo = String.format("data/%s.txt",
                 obtenerNombreArchivo());;
     }
-
     public String obtenerNombreArchivo() {
         return nombreArchivo;
     }
-
     public String obtenerRutaArchivo() {
         return rutaArchivo;
     }
-
     // establecer valores de cada línea
     // en la lista de tipo Profesor
     public void establecerLista() {
         lista = new ArrayList<>();
         File f = new File(rutaArchivo);
-
         if (f.exists()) {
-
             while (entrada.hasNext()) {
                 String linea = entrada.nextLine(); // Tara Hernandez;contratado
-
                 ArrayList<String> linea_partes = new ArrayList<>(
                         Arrays.asList(linea.split(";")) // ["Tara Hernandez", "contratado"]
                 );
@@ -73,23 +54,17 @@ public class ArchivoLectura {
                         linea_partes.get(1) // contratado
                 );
                 lista.add(p);
-
             } // fin de while
         }
     }
-
     public ArrayList<Profesor> obtenerLista() {
-
         return lista;
     }
-
     public void cerrarArchivo() {
         if (entrada != null) {
             entrada.close();
         } // cierra el archivo
-
     }
-
     @Override
     public String toString() {
         String cadena = "Lista Profesores\n";
